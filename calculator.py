284590362478af422e989cbe61f9b1db51f7d3da
rarayach <rica.arayac@duocuc.cl>
2026-06-05 09:49:19 -0400

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,54 @@
+calculadora
+
+def sumar():
+    
+def restar():
+    
+def Multiplicar():
+    
+def dividir():
+    
+def potencia():
+    
+def raiz():
+    
+def Salir():
+    
+    
+
+
+
+
+print("1) sumar")
+print("2) restar")
+print("3) Multiplicar")
+print("4) dividir")
+print("5) potencia")
+print("6) raiz")
+print("7) Salir")
+
+opcion =int(input("Escoga una opcion"))
+
+match opcion:
+    case 1:
+        print("suma")
+    
+    case 2:
+        print("resta")
+    
+    case 3:
+        print("Multiplicar")
+    
+    case 4:
+        print("dividir")
+    
+    case 5:
+        print("potencia")
+    
+    case 6:
+        print("raiz")
+    case 7:
+        print("salir")
+        
+    case _:
+        print("Eror")
